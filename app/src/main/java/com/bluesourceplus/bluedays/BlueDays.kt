b17d6a808712df7ae7879fd8c8513acef78b1829
Sergey Hayriyan <sega1ness@gmail.com>
2025-01-01 02:38:30 +0400

    draft setup

--- app/src/main/java/com/bluesourceplus/bluedays/BlueDays.kt
+++ app/src/main/java/com/bluesourceplus/bluedays/BlueDays.kt
@@ -4,15 +4,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.bluesourceplus.bluedays.composables.BluedaysTheme
+import org.koin.compose.KoinContext
+import org.koin.core.context.startKoin
 
 class BlueDays : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        startKoin {
+
+        }
+
         setContent {
             BluedaysTheme {
-
+                KoinContext {
+                    val navController = rememberNavController()
+                    val snackbarHostState = remember { SnackbarHostState() }
+                    Scaffold(
+                        bottomBar = { BottomBar(navController) },
+                        snackbarHost = { SnackbarHost(snackbarHostState) },
+                        modifier = Modifier.fillMaxSize(),
+                        contentWindowInsets = WindowInsets(top = 0.dp),
+                    ) { innerPadding ->
+                        BlueDaysScreensHost(navController, innerPadding)
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/bluesourceplus/bluedays/BlueDaysScreensHost.kt
@@ -0,0 +1,142 @@
+package com.bluesourceplus.bluedays
+
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.bluesourceplus.bluedays.feature.create.CreateScreenRoute
+import com.bluesourceplus.bluedays.feature.home.HomeScreenRoute
+import com.bluesourceplus.bluedays.feature.preferences.PreferencesScreenRoute
+
+@Composable
+fun BlueDaysScreensHost(
+    navController: NavHostController,
+    padding: PaddingValues,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Destination.Home.route,
+        modifier =
+        Modifier
+            .padding(padding)
+            .fillMaxSize(),
+    ) {
+        appScreen(Destination.Home) {
+            HomeScreenRoute()
+        }
+
+        appScreen(Destination.Preferences) {
+            PreferencesScreenRoute()
+        }
+
+        appScreen(Destination.Create) {
+            CreateScreenRoute()
+        }
+    }
+}
+
+@Composable
+internal fun BottomBar(navController: NavController) {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 20.dp, horizontal = 40.dp)
+            .background(color = MaterialTheme.colorScheme.onBackground, shape = RoundedCornerShape(60.dp)))
+    {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+
+        AppNavigationItem(
+            rootScreen = Destination.Home,
+            currentDestination = currentDestination,
+            navController = navController,
+            content = { Icon(imageVector = Icons.Default.Home, contentDescription = stringResource(R.string.home_button_content_description), tint = MaterialTheme.colorScheme.onSecondaryContainer) },
+        )
+        AppNavigationItem(
+            rootScreen = Destination.Preferences,
+            currentDestination = currentDestination,
+            navController = navController,
+            content = { Icon(imageVector = Icons.Default.Settings, contentDescription = stringResource(R.string.preferences_button_content_description), tint = MaterialTheme.colorScheme.onSecondaryContainer) },
+        )
+    }
+}
+
+@Composable
+private fun RowScope.AppNavigationItem(
+    rootScreen: Screen,
+    currentDestination: NavDestination?,
+    navController: NavController,
+    content: @Composable () -> Unit,
+) {
+    NavigationBarItem(
+        selected = currentDestination?.hierarchy?.any { it.route == rootScreen.route } == true,
+        onClick = {
+            navController.navigate(rootScreen.route) {
+                popUpTo(navController.graph.findStartDestination().id)
+                launchSingleTop = true
+            }
+        },
+        icon = content,
+    )
+}
+
+sealed class Screen(
+    val route: String,
+    val arguments: List<NamedNavArgument> = emptyList(),
+)
+
+fun NavGraphBuilder.appScreen(
+    screen: Screen,
+    content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit,
+) {
+    composable(
+        route = screen.route,
+        arguments = screen.arguments,
+        content = content,
+    )
+}
+
+const val CREATE_SCREEN_ROUTE = "create"
+const val HOME_SCREEN_ROUTE = "home"
+const val PREFERENCES_SCREEN_ROUTE = "preferences"
+
+object Destination {
+    data object Create : Screen(
+        route = CREATE_SCREEN_ROUTE,
+    )
+
+    data object Home : Screen(
+        route = HOME_SCREEN_ROUTE,
+    )
+
+    data object Preferences : Screen(
+        route = PREFERENCES_SCREEN_ROUTE,
+    )
+}
