04d6dddecf1d21fa76c11b316ec413c1e4a5353c
Sergey Hayriyan <sega1ness@gmail.com>
2025-01-01 01:26:02 +0400

    deleted standard code, renamed entry

---
+++ app/src/main/java/com/bluesourceplus/bluedays/BlueDays.kt
@@ -0,0 +1,19 @@
+package com.bluesourceplus.bluedays
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.bluesourceplus.bluedays.ui.theme.BluedaysTheme
+
+class BlueDays : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BluedaysTheme {
+
+            }
+        }
+    }
+}
