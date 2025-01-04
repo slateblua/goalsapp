8d0a76fd78628762f6b9f22fd8fead16c55b0ea2
Sergey Hayriyan <sega1ness@gmail.com>
2025-01-04 04:09:37 +0400

    component

---
+++ app/src/main/java/com/bluesourceplus/bluedays/composables/system/BlueButton.kt
@@ -0,0 +1,12 @@
+package com.bluesourceplus.bluedays.composables.system
+
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BlueButton (modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Button (modifier = Modifier, onClick = onClick) {
+        
+    }
+}
