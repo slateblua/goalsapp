4ec7d0be68c808062929faae9e3e3ffa7206571e
Sergey Hayriyan <sega1ness@gmail.com>
2025-01-12 19:56:38 +0400

    DeleteGoalUseCase

--- app/src/main/java/com/bluesourceplus/bluedays/feature/aboutgoalscreen/module/AboutModule.kt
+++ app/src/main/java/com/bluesourceplus/bluedays/feature/aboutgoalscreen/module/AboutModule.kt
@@ -1,6 +1,8 @@
 package com.bluesourceplus.bluedays.feature.aboutgoalscreen.module
 
 import com.bluesourceplus.bluedays.feature.aboutgoalscreen.AboutGoalViewModel
+import com.bluesourceplus.bluedays.feature.aboutgoalscreen.usecases.DeleteGoalUseCase
+import com.bluesourceplus.bluedays.feature.aboutgoalscreen.usecases.DeleteGoalUseCaseImpl
 import com.bluesourceplus.bluedays.feature.aboutgoalscreen.usecases.GetGoalByIdUseCase
 import com.bluesourceplus.bluedays.feature.aboutgoalscreen.usecases.GetGoalByIdUseCaseImpl
 import org.koin.core.module.dsl.viewModel
@@ -11,4 +13,6 @@ val aboutModule = module {
     viewModel { AboutGoalViewModel() }
 
     single { GetGoalByIdUseCaseImpl(get()) } bind GetGoalByIdUseCase::class
+
+    single { DeleteGoalUseCaseImpl(get()) } bind DeleteGoalUseCase::class
 }
