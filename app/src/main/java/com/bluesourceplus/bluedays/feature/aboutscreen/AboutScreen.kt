99fa86984d3fe1458d9f0f27ace322ef1c32c509
Sergey Hayriyan <sega1ness@gmail.com>
2025-01-15 01:15:22 +0400

    relayout

--- app/src/main/java/com/bluesourceplus/bluedays/feature/aboutscreen/AboutScreen.kt
+++ app/src/main/java/com/bluesourceplus/bluedays/feature/aboutscreen/AboutScreen.kt
@@ -9,10 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Code
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,7 +39,11 @@ fun AboutScreenRoute() {
 
 @Composable
 fun AboutScreen(modifier: Modifier = Modifier) {
-    Box(modifier = modifier.fillMaxSize().padding(10.dp)) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(10.dp)
+    ) {
         Column(
             modifier = modifier.align(Alignment.Center),
             horizontalAlignment = Alignment.CenterHorizontally
@@ -43,12 +53,12 @@ fun AboutScreen(modifier: Modifier = Modifier) {
                 contentDescription = stringResource(R.string.app_name),
                 modifier =
                 Modifier
-                    .size(80.dp),
+                    .size(50.dp),
             )
             Spacer(modifier = Modifier.height(16.dp))
             Text(
                 text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineLarge,
+                style = MaterialTheme.typography.headlineMedium,
                 fontWeight = FontWeight.Bold,
             )
             Spacer(modifier = Modifier.height(8.dp))
@@ -58,26 +68,17 @@ fun AboutScreen(modifier: Modifier = Modifier) {
                 style = MaterialTheme.typography.bodyMedium,
                 color = Color.Gray,
             )
-            Spacer(modifier = Modifier.height(24.dp))
-
-            Card(
-                modifier = Modifier.fillMaxWidth(),
-                elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
-            ) {
-                Text(
-                    text = "Stay tuned for more!",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(16.dp),
-                )
-            }
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(8.dp))
 
             val localUriHandler = LocalUriHandler.current
 
-            OutlinedButton(onClick = {
+            Button(onClick = {
                 localUriHandler.openUri("https://github.com/slateblua")
             }) {
-                Text(text = "GitHub")
+                Icon(
+                    imageVector = Icons.Filled.Code,
+                    contentDescription = "GitHub",
+                )
             }
         }
     }
